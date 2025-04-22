8c8753da7da03ab4d03c441514173f6469604e46
Escape <Escape>
2025-04-22 15:47:56 +0530

    commit made

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
-'use client'; // Make sure this is at the top!
+'use client';
 
 import { useRouter } from 'next/navigation';
+import { Button } from '@mui/material';
 
 export default function Home() {
   const router = useRouter();
@@ -11,12 +12,12 @@ export default function Home() {
 
   return (
     <div className="p-4"> 
-      <button
+      <Button
         onClick={goToHomePage}
         className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
       >
         Go to Home Page
-      </button>
+      </Button>
     </div>
   );
 }
